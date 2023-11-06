b7d58adf6042cb2fdcb9fecd96ab4e0ba0c24b0a
Jona <llamasjonat@gmail.com>
2023-11-05 23:33:12 -0300

    fix(component): structure for ValueItem for ValueProposal

--- src/components/ValueProposalSection/ValueItem/index.tsx
+++ src/components/ValueProposalSection/ValueItem/index.tsx
@@ -3,27 +3,17 @@ import { Text } from '@/components/UI'
 import { Value } from './style'
 
 interface ComponentProps {
-  image?: string
   title: string
   description: string
-  url: string
 }
 
 export default function Component(props: ComponentProps) {
-  const {
-    image = 'https://placehold.co/50x50',
-    title,
-    description,
-    url = '#'
-  } = props
+  const { title, description } = props
 
   return (
     <Value>
-      <img src={image} />
-      <div>
-        <Text isBold>{title}</Text>
-        <Text>{description}</Text>
-      </div>
+      <Text isBold>{title}</Text>
+      <Text>{description}</Text>
     </Value>
   )
 }
--- src/components/ValueProposalSection/ValueItem/style.tsx
+++ src/components/ValueProposalSection/ValueItem/style.tsx
@@ -4,32 +4,15 @@ import theme from '@/style/theme'
 
 export const Value = styled.div`
   display: flex;
-  align-items: center;
-  gap: 12px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 4px;
   min-width: 325px;
   max-width: 325px;
 
-  padding: 16px;
+  padding: 16px 24px;
 
   border: 1px solid ${theme.colors.gray};
   border-radius: 24px;
-
-  img {
-    width: 50px;
-    height: 50px;
-
-    border-radius: 50px;
-  }
-
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-
-    text-align: left;
-
-    p {
-      font-size: 16px;
-    }
-  }
 `
