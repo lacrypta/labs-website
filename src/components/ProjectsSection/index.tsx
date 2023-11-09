07671485ca42670add157ec4d60a43989568af18
Jona <llamasjonat@gmail.com>
2023-11-09 19:30:09 -0300

    fix(component): cleaned structure for ProjectsSection

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -14,33 +14,21 @@ export default function Component() {
       <Container>
         <Header>
           <Heading as="h2">Creemos en el Open-Source.</Heading>
-          <Text>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
-            omnis! Sequi ex commodi cumque hic.
-          </Text>
-          <Link href="#">Ver proyectos de la comunidad</Link>
+          {/* <Text>Todos nuestros proyectos son completamente libres para </Text>
+          <Link href="#">Ver proyectos de la comunidad</Link> */}
         </Header>
-        <Project
-          // image=""
-          title="LaWallet"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit ipsum
-            facilis fuga aliquam possimus rem."
-          url=""
-        />
       </Container>
       <Container>
         <Project
-          // image=""
-          title="POS"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit ipsum
-            facilis fuga aliquam possimus rem."
+          image="/img/projects/wallet.png"
+          title="LaWallet"
+          description=""
           url=""
         />
         <Project
-          // image=""
-          title="Peronio"
-          description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit ipsum
-            facilis fuga aliquam possimus rem."
+          image="/img/projects/pos.png"
+          title="POS"
+          description=""
           url=""
         />
       </Container>
