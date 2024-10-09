fa3fbc692799ee53baaf56b55b4f477ccfd92b50
Jona <llamasjonat@gmail.com>
2024-10-09 14:11:11 -0300

    chore: cleaned components

--- src/components/UI/Icon/style.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components'
-
-export const Icon = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 24px;
-  height: 24px;
-`
