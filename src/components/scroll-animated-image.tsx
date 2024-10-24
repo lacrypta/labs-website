8cd46b077f8014826a390a10f0f4a1682434bd03
Jona <llamasjonat@gmail.com>
2024-10-24 16:22:41 -0300

    feat: added ScrollAnimatedImage component

---
+++ src/components/scroll-animated-image.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useRef, useEffect, useState } from 'react'
+import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
+import { useScreenDetector } from '@/hooks/use-screen-detector'
+
+interface ScrollAnimatedImageProps {
+  src: string
+  alt: string
+}
+
+export function ScrollAnimatedImage({ src, alt }: ScrollAnimatedImageProps) {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [containerHeight, setContainerHeight] = useState(0)
+
+  const { isMobile } = useScreenDetector()
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end start']
+  })
+
+  const scale = useTransform(scrollYProgress, [-1, 1], [0.5, 1])
+  const opacity = useTransform(scrollYProgress, [0, 0.2, 1], [0, 1, 1])
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, 1]
+    // [isMobile ? '-20%' : '30%', isMobile ? '50%' : '-10%']
+  )
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setContainerHeight(containerRef.current.offsetHeight)
+    }
+  }, [])
+
+  return (
+    <div
+      ref={containerRef}
+      className="relative w-full"
+      // style={{ height: `${containerHeight}px` }}
+    >
+      <motion.div
+        className="sticky overflow-hidden top-0 -md:top-32 aspect-video flex items-center justify-center"
+        style={{ scale, opacity, translateY: y }}
+      >
+        <motion.div className="relative overflow-hidden w-full h-full rounded-xl">
+          <Image
+            src={src}
+            alt={alt}
+            fill
+            className="object-contain md:object-cover pointer-events-none rounded-xl"
+            priority
+          />
+        </motion.div>
+      </motion.div>
+    </div>
+  )
+}
