cc688bb18d872b7729a10b67d7b00d3d0729f7a5
fierillo <fierillo@protonmail.com>
2025-06-21 16:18:57 -0300

    feat: add 'Our Projects' section to translations

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -6,72 +6,32 @@ import Container from '../Layout/Container'
 import Project from './Project'
 import { cn } from '@/lib/utils'
 
-const MOCK_PROJECTS = [
+const PROJECTS_SKELETON = [
   {
+    id: 'lawallet', 
     image: '/img/projects/lawallet.png',
-    title: 'LaWallet',
-    description:
-      'We create infrastructure based on Lightning Network and Nostr to speed up your integration.',
     url: 'https://lawallet.io',
-    features: [
-      {
-        icon: '',
-        title: 'Infraestructure',
-        description:
-          'Integrating Bitcoin should not be utopian. Build on a solid and flexible infrastructure, ensuring security and scalability.'
-      },
-      {
-        icon: '',
-        title: 'Feature o característica',
-        description: 'Breve descripción de esa feature o característica.'
-      }
-    ]
+    featureCount: 2 
   },
   {
+    id: 'walias',
     image: '/img/projects/walias.png',
-    title: 'Walias',
-    description:
-      'Merge the power of Lightning and Nostr to make Bitcoin as simple as sending an email.',
     url: 'https://walias.io/',
-    features: [
-      {
-        icon: '',
-        title: 'Feature o característica',
-        description: 'Breve descripción de esa feature o característica.'
-      },
-      {
-        icon: '',
-        title: 'Feature o característica',
-        description: 'Breve descripción de esa feature o característica.'
-      }
-    ]
+    featureCount: 2
   },
   {
+    id: 'halving_massacre',
     image: '/img/projects/halving_massacre.png',
-    title: 'Halving Massacre',
-    description:
-      'Zap sats to boost your bolt power, survive random elimination rounds, and claim the prize pool by being the last player standing!',
     url: 'https://massacre.lawallet.io/',
-    features: [
-      {
-        icon: '',
-        title: 'Feature o característica',
-        description: 'Breve descripción de esa feature o característica.'
-      },
-      {
-        icon: '',
-        title: 'Feature o característica',
-        description: 'Breve descripción de esa feature o característica.'
-      }
-    ]
+    featureCount: 2
   }
 ]
 
 export default function Component() {
   const t = useTranslations('projects')
+
   return (
     <div className="flex flex-col gap-16 w-full pt-64 pb-16">
-      {/* Quienes somos */}
       <Container>
         <div className="flex flex-col gap-4 md:max-w-[500px] mx-auto text-center">
           <h2 className="text-4xl lg:text-7xl font-bold">{t('title')}</h2>
@@ -80,16 +40,33 @@ export default function Component() {
       </Container>
       <Container>
         <div className="flex flex-col gap-16">
-          {MOCK_PROJECTS.map((project, index) => (
-            <Project
-              className={cn(
-                'flex flex-col gap-8 w-full p-6 rounded-3xl bg-border',
-                index % 2 ? 'lg:flex-row' : 'lg:flex-row-reverse'
-              )}
-              key={index}
-              data={project}
-            />
-          ))}
+          {PROJECTS_SKELETON.map((project, index) => {
+            const projectData = {
+              image: project.image,
+              url: project.url,
+              title: t(`items.${project.id}.title`),
+              description: t(`items.${project.id}.description`),
+              features: Array.from({ length: project.featureCount }).map(
+                (_, featureIndex) => ({
+                  icon: '', 
+                  title: t(`items.${project.id}.features.${featureIndex}.title`),
+                  description: t(
+                    `items.${project.id}.features.${featureIndex}.description`
+                  )
+                })
+              )
+            }
+            return (
+              <Project
+                className={cn(
+                  'flex flex-col gap-8 w-full p-6 rounded-3xl bg-border',
+                  index % 2 ? 'lg:flex-row' : 'lg:flex-row-reverse'
+                )}
+                key={project.id}
+                data={projectData} 
+              />
+            )
+          })}
         </div>
       </Container>
     </div>
