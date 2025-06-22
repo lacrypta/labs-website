d3f644e75836b842a4d825c9bb35c775d930b792
Agustin Kassis <webmaster@masize.com>
2025-06-22 19:20:30 +0200

    move html data into layout and remove _document

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -55,6 +55,37 @@ export default async function RootLayout({
         <meta name="twitter:image" content="/img/social/twitter-600x330.jpg" />
         <meta name="twitter:url" content="https://lacrypta.ar/" />
         <script async src="https://tally.so/widgets/embed.js"></script>
+        <script
+          id="ldjson-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: `
+              {
+                "@context": "https://schema.org",
+                "@type": "EducationalOrganization",
+                "name": "La Crypta",
+                "alternateName": "LaCrypta",
+                "url": "https://lacrypta.ar/",
+                "logo": "https://lacrypta.ar/img/schema-logo.png",
+                "sameAs": [
+                  "https://twitter.com/LaCryptaOk",
+                  "https://www.instagram.com/lacryptaok/",
+                  "https://www.linkedin.com/company/lacryptaok/",
+                  "https://github.com/lacrypta",
+                  "https://lacrypta.ar/"
+                ],
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Villanueva 1367",
+                  "addressLocality": "Belgrano",
+                  "postalCode": "C1426",
+                  "addressCountry": "AR",
+                  "addressRegion": "CABA, Buenos Aires"
+                }
+              }
+            `
+          }}
+        />
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
         />
